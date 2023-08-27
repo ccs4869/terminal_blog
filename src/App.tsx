056a364e232e95cdaa7ef10483caae5c6ba23c95
canse <1300962245@qq.com>
2023-08-28 00:47:41 +0800

    react init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>React</p>
+    </>
+  )
+}
+
+export default App
